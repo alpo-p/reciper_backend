edabf3723979621075117d72657e3a391c63e8ec
supea <alpo.panula@gmail.com>
2021-05-10 12:00:50 +0300

    initialized backend

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
